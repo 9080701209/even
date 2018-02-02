93654614a883f5c6406a066cde929c1d51fccba6
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-02 15:48:14 +0530

    Update 26.c

--- set3/26.c
+++ set3/26.c
@@ -1,48 +1,38 @@
 #include<stdio.h>
-void quicksort(int number[25],int first,int last){
+void quicksort(int num[25],int first,int last){
    int i, j, pivot, temp;
-
    if(first<last){
       pivot=first;
       i=first;
       j=last;
-
       while(i<j){
-         while(number[i]<=number[pivot]&&i<last)
+         while(num[i]<=num[pivot]&&i<last)
             i++;
-         while(number[j]>number[pivot])
+         while(num[j]>num[pivot])
             j--;
          if(i<j){
-            temp=number[i];
-            number[i]=number[j];
-            number[j]=temp;
+            temp=num[i];
+            num[i]=num[j];
+            num[j]=temp;
          }
       }
-
-      temp=number[pivot];
-      number[pivot]=number[j];
-      number[j]=temp;
-      quicksort(number,first,j-1);
-      quicksort(number,j+1,last);
-
+ temp=num[pivot];
+      num[pivot]=num[j];
+      num[j]=temp;
+      quicksort(num,first,j-1);
+      quicksort(num,j+1,last);
    }
 }
-
 int main(){
-   int i, count, number[25];
-
+   int i, count, num[25];
    printf("How many elements are u going to enter?: ");
    scanf("%d",&count);
-
    printf("Enter %d elements: ", count);
    for(i=0;i<count;i++)
-      scanf("%d",&number[i]);
-
-   quicksort(number,0,count-1);
-
+      scanf("%d",&num[i]);
+   quicksort(num,0,count-1);
    printf("Order of Sorted elements: ");
    for(i=0;i<count;i++)
-      printf(" %d",number[i]);
-
+      printf(" %d",num[i]);
    return 0;
 }
