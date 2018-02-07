bcabfc404522d2b2ab64743a175974aba8de1db8
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-07 16:20:30 +0530

    Update 36.c

--- set3/36.c
+++ set3/36.c
@@ -1,17 +1,20 @@
 #include<stdio.h>
-#include<conio.h>
 int main()
 {
- int i,c=0;   
-    char a[50]="thahira is badgirl 123";
-    for(i=0;a[i]!='\0';i++)
+ int i,c=0,spl=0,c1=0;   
+    char a[50]="thahirabanu123$%$";
+for(i=0;a[i]!='\0';i++)
     {
-        if(a[i]>='a'&&a[i]<='z'||a[i]>='A'&&A[I]<='Z')
-        {
+        if(a[i]>='0' && a[i]<='9')
+            c1++;
+        else if((a[i]>='a' && a[i]<='z')||(a[i]>='A' && a[i]<='Z'))
             c++;
-        }
+        else
+            spl++;
     }
-    printf("%d",c);
+    printf("%d\n",spl);
+    printf("%d\n",c);
+    printf("%d\n",c1);
     return 0;
-    }
+    
 }
