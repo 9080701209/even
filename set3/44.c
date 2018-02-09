81e8c8aa0aff9aece0da3ee8390f7e34efccc82e
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-09 15:59:35 +0530

    Create 44.c

---
+++ set3/44.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int n=5,a=1,b=10;
+    if(n>a && n<b)
+    {
+        printf("n is in the range of 1,10");
+    }
+    else
+    {
+        printf("n is not in the range of 1,10");
+    }
+    return 0;
+}
