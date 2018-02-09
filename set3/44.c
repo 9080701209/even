faa6e6de98c806368f6da38ed697446c77ae5fbe
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-09 16:01:53 +0530

    Update 44.c

--- set3/44.c
+++ set3/44.c
@@ -4,11 +4,11 @@ int main()
     int n=5,a=1,b=10;
     if(n>a && n<b)
     {
-        printf("n is in the range of 1,10");
+        printf("yes");
     }
     else
     {
-        printf("n is not in the range of 1,10");
+        printf("no");
     }
     return 0;
 }
