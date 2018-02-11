efe4789d151024010e1726a114248f9c955ecf6c
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-11 11:08:02 +0530

    Create 53.c

---
+++ set3/53.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void main()
+{
+    int num,  digit, sum;
+ printf("Enter the number \n");
+scanf("%d", &num);
+while (num > 0
+{
+digit = num % 10;
+  sum  = sum + digit;
+   num /= 10;
+    }
+printf("Sum of the digits %d ",  sum);
+}
