82ace09577844c8d55236134ccab1a09def211d1
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-12 16:54:22 +0530

    Create 64.c

---
+++ set3/64.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,c;
+    scanf("%d",&a);
+    scanf("%d",&b);
+     c=a+b;
+     if(c%2==0)
+     {
+         printf("the result is even");
+     }
+    else
+    {
+        printf("the result is odd");
+    }
+}
