4900f3290b934d90b86b7308e54080239dbc82b0
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-12 16:36:22 +0530

    Create 61.c

---
+++ 61.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char a[50];
+    int n,i;
+    scanf("%s",a);
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+   printf("%c",a[i]);
+    }
+         
+}
