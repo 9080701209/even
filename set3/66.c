83f2674e695139e49d1dca0ce1219b80cb6baa26
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-19 15:53:31 +0530

    Update 66.c

--- set3/66.c
+++ set3/66.c
@@ -1,24 +1,32 @@
 #include<stdio.h>
+
 int main()
 {
-int n,i,flag=0;
-printf("enter positive integer");
-scanf("%d",&n);
-for(i=2;i<+n/2;++i)
-{
-if(n%2==0)
-{
-flag=1;
-break;
-}
-}
-if(flag==2)
-{
-printf("&d is a prime",n);
-}
-else
-{
-printf("%d is not a prime",n);
-}
-return 0;
-}
+	int n,i;
+	scanf("%d",&n);
+	if(n==0)
+	{
+		printf("enter valid input ");
+	}
+	else if(n==1)
+	{
+		printf("1 is neither prime nor composite");
+	}
+	else
+	{
+		for(i=2;i<=n;i++)
+		{
+			if(n%i==0 & n!=i)
+			{
+				printf("not prime");
+				break;
+				
+			}
+			 if(n%i==0 & n==i)
+			 {
+			 	printf("prime");
+			 }
+		}
+	}
+	return 0;
+	}
