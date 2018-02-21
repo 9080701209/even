9e40831de6078f9c399ed54ae296ecf180274edf
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-21 16:50:30 +0530

    Update 79.c

--- set3/79.c
+++ set3/79.c
@@ -1,17 +1,21 @@
-#include<stdio.h>
+ #include <stdio.h>
+
 int main()
 {
-    int a,b,c;
-    scanf("%d %d",&a,&b);
-    c=a*b;
-    printf("%d",c);
-    if(a==b)
-    {
-        printf("yes");
-    }
-    else
-    {
-        printf("no");
-    }
-return 0;
-    }
+	int a,b,c,i,p,flag=0;
+	scanf("%d %d",&a,&b);
+	c=b*a;
+	for(i=1;i<=c/2;i++)
+	{
+		p=i*i;
+		if(p==c)
+		{
+			printf("yes");
+			flag=1;
+			break;
+		}
+	}
+	if(flag==0)
+	printf("no");
+	return 0;
+}
