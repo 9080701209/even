7134396cfb55de8221605bdb164079e57019db74
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-28 16:35:13 +0530

    Create 92.c

---
+++ set3/92.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int N,i,n,sum=0;
+scanf("%d",&N);
+for(i=0;i<N;i++)
+{
+    scanf("%d",&n);
+    sum=sum+n;
+}
+printf("sum of n numbers is %d ",sum);
+    return 0;
+}
+    
+    
