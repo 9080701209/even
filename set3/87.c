56427dffa39afc02ea7de09e6ac2abe1e5b94def
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-02-28 15:49:22 +0530

    Create 87.c

---
+++ set3/87.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int  main()
+{
+    int a,b,i,gcd;
+    printf("enter value of a and b\n");
+    scanf("%d %d",&a,&b);
+    for(i=1;i<=a && i<=b;++i)
+    {
+        if(a%i==0 && b%i==0)
+        {
+             gcd=i;
+        }
+    }
+       printf("\n GCD OF number %d and %d  is  %d",a,b,gcd);
+       return 0;
+}
