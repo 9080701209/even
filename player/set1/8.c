18151d9a5854304b967f86167e5142331c510c8c
9080701209 <31505908+9080701209@users.noreply.github.com>
2018-03-19 16:13:39 +0530

    Update 8.c

--- player/set1/8.c
+++ player/set1/8.c
@@ -1,18 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
 #include<string.h>
-int main() 
+int main()
 {
-	char n[100];
-    int a,i;
-    scanf("%[^\t\n]s",n);
-    a=strlen(n);
-    n[0]=n[0]-32;
-    for(i=0;i<a;i++)
+    char a[100];
+    int i,m;
+    scanf("%[^\n]s",a);
+     m=strlen(a);
+    if(a[0]>96&&a[0]<123)
     {
-      if(n[i]==' ')
-      {
-          n[i+1]=n[i+1]-32;
-      }
+        a[0]-=32;
     }
-    printf("%s",n);
+    for(i=1;i<=m;i++)
+    {
+        if(a[i]==' ')
+        {
+            i++;
+            if(a[i]>96&&a[i]<123)
+            {
+                a[i]-=32;
+            }
+        }
+        else
+        {
+            if(a[i]>64&&a[i]<91)
+            {
+                a[i]+=32;
+            }
+        }
+    }
+    printf("%s",a);
+    return 0;
 }
